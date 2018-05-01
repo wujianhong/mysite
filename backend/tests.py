50cc96a0853a434ec7b524ce66d7dfc2f0a58ebe
996267036@qq.com <wujianhong>
2018-05-02 00:23:54 +0800

    second commit

--- backend/tests.py
+++ backend/tests.py
@@ -4,29 +4,48 @@ from __future__ import unicode_literals
 from django.test import TestCase
 import random
 import string
-# Create your tests here.
-# 选项
+
 list = string.ascii_uppercase
 
 # 权重，升序排列
 weighted = range(1, 27)
 
-def weighted_random(list, weighted, num):
-    # 从1到权重之和间随机抽取一个整数
-    r = random.randint(1, sum(weighted))
+b = []
+a = []
+for idx, tmp in enumerate(list):
+    for i, w in enumerate(weighted):
+        if idx == i:
+            a += [tmp] * w
+            print(a)
 
-    print(r)
-    # 把权重依此相加，直到大于或等于随机数r，相加的次数记为n，跳出循环
-    # 从选项列表中取 n-1 位作为输出的加权随机数
-    c = 0
-    for index, w in enumerate(weighted):
-        c = c + w
-        if c >= r:
-            return (list[index])
-            break
-all = []
-for i in range(700):
-    all.append(weighted_random(list, weighted, 8))
-print('A:', all.count("A"))
-print('B:', all.count("B"))
-print('C:', all.count("C"))
+
+def get_count(b):
+    result = {}
+    for tmp in b:
+        num = b.count(tmp)
+        if num > 1:
+            result[tmp] = num - 1
+    return result
+
+def remove(li, di):
+    result = li[:]
+    for key, value in di.items():
+        for tmp in range(value):
+            result.remove(key)
+    return result
+
+def remove_src(li, di):
+    result = li[:]
+    for key in di:
+        num = result.count(key)
+        for i in range(num):
+            # print(key, result)
+            result.remove(key)
+    return result
+
+for i in range(50):
+    b = random.sample(a, 10)
+    tmp = get_count(b)
+    r = remove(b, tmp)
+    s = remove_src(a, r)
+    print(b, tmp, r, s)
