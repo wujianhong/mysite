44429c67f76d8b54b06b94fd218883d3d180df6e
wujianhong <996267036@qq.com>
2018-05-02 03:43:38 +0000

    Update views.py

--- backend/views.py
+++ backend/views.py
@@ -3,49 +3,47 @@ from __future__ import unicode_literals
 from django.http import JsonResponse
 import json
 from django.shortcuts import render
-from backend.models import Employee
+from backend.models import Employee, Person
 import pandas as pd
 # Create your views here.
 import numpy as np
-def index(request):
+import randomimport string
+
+from datetime import datetime
+def index(request):    
     return render(request, 'home.html')
-
-def _map(data):
-    result = []
-
-    for index, row in data.iterrows():   # 获取每行的index、row
-        tmp_row = []
-        for col_name in data.columns:
-            tmp_row.append(row[col_name])
-        result.append(tmp_row)
-        print(tmp_row)
+def _map(data):    
+    result = []    
+    for index, row in data.iterrows():   # 获取每行的index、row        
+        tmp_row = []        
+        for col_name in data.columns:            
+            tmp_row.append(row[col_name])        
+        result.append(tmp_row)        
+        # print(tmp_row)    
     return result
 
-def onload(request):
-    file = request.FILES.get('excel')
-    df = pd.read_excel(request.FILES.get('excel'))
-    print(df.items())
-    print(df.columns)
-    temp = _map(df)
-    print(temp)
+def remove_src(li, di):    
+    result = li[:]    
+    for key in di:        
+        num = result.count(key)        
+        for i in range(num):                # print(key, result)            
+            result.remove(key)    
+    return result
 
-    for row in temp:
-        tmp_emp = Employee(name=row[0],weight=row[1],is_trainee=row[2])
-        tmp_emp.save()
-    return render(request, 'excel.html')
-
-def getEmployee(request):
-    num = request.GET['num']
-    emp_list = Employee.objects.filter(is_enabled=1)
-    for emp in emp_list:
-        emp.name
-    return JsonResponse()
-
-def excel(request):
-    return render(request, 'excel.html')
-
-def get_user(request):
-    name_dict = {'twz': 'Love python and Django', 'zqxt': 'I am teaching Django'}
-    emp = Employee(name="吴键鸿")
-    emp.save()
-    return JsonResponse(name_dict)
+def getPerson(request):    
+    person_list = Person.objects.all()    
+    src_list = []    
+    for person in person_list:        
+        src_list.append(person.name)    
+        num = int(request.GET['num'])    
+        b = random.sample(src_list, num)    
+        # print(type(b))    
+        r = list(set(b))    
+        tmp_list = src_list[:]    
+        while len(r) < num:        
+            tmp_list = remove_src(tmp_list, r)        
+            print(r, tmp_list)        
+            tmp_r = random.sample(tmp_list, num - len(r))        
+            r = list(set(r + tmp_r))        
+            print(r, tmp_list)    
+    return JsonResponse(r, safe=False)
